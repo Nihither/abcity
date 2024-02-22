7f67f101865d06c16bc80f11e5e69002c930b947
Andrew Semenov <semenov.an.sh@gmail.com>
2024-02-23 00:37:25 +0300

    add Video model

--- abcity/settings.py
+++ abcity/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = local_settings.ALLOWED_HOSTS[ENV]
 # Application definition
 
 INSTALLED_APPS = [
+    'content.apps.ContentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- content/models.py
+++ content/models.py
@@ -1,3 +1,12 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Название")
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Описание")
+    video = models.FileField(upload_to='video', verbose_name="Видео")
+    created = models.DateTimeField(verbose_name="Создано", default=datetime.now)
+
+    def __str__(self):
+        return self.name
