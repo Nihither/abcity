1cad469d86a6825581b3f4d66062cbb2e92c78b4
Andrew Semenov <semenov.an.sh@gmail.com>
2024-02-24 13:13:27 +0300

    add settings for prod

--- abcity/settings.py
+++ abcity/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = os.path.join(STATIC_BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = local_settings.MEDIA_ROOT[ENV]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
